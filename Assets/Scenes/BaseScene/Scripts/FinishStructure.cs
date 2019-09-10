462e8c57edfdd9051641996c22d1a8d4b4225627
Jakov Jelenić <jakov.jelenic@fer.hr>
2019-09-10 17:11:55 +0200

    added courutine

--- Assets/Scenes/BaseScene/Scripts/FinishStructure.cs
+++ Assets/Scenes/BaseScene/Scripts/FinishStructure.cs
@@ -10,19 +10,7 @@ public class FinishStructure : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        ObjectPairList other = GameObject.Find("SceneManager").GetComponent(typeof(ObjectPairList)) as ObjectPairList;
-        StructureInfo script = this.GetComponent<StructureInfo>();
-        Debug.Log("script.Name:" + script.Name + ";;" + script.name);
-        GameObject loadedObject = other.returnGameObject(script.Name);
-        Vector2 structurePosition = new Vector2(script.X, script.Y);
-        Debug.Log("loadedObject:" + loadedObject.name);
-        GameObject instantiatedObject = Instantiate(loadedObject, structurePosition, Quaternion.identity);
-        StructureInfo script2 = instantiatedObject.GetComponent<StructureInfo>();
-        script2.X = (int)structurePosition.x;
-        script2.Y = (int)structurePosition.y;
-        //script.name = structure.name;
-        script2.Level = script.Level;
-        Destroy(gameObject);
+        StartCoroutine(spawnObject());
         
 
     }
@@ -31,5 +19,23 @@ public class FinishStructure : MonoBehaviour
     void Update()
     {
         
+    }
+
+    IEnumerator spawnObject()
+    {
+        ObjectPairList other = GameObject.Find("SceneManager").GetComponent(typeof(ObjectPairList)) as ObjectPairList;
+        StructureInfo script = this.GetComponent<StructureInfo>();
+        Debug.Log("script.Name:" + script.Name + ";;" + script.name);
+        GameObject loadedObject = other.returnGameObject(script.Name);
+        Vector2 structurePosition = new Vector2(script.X, script.Y);
+        Debug.Log("loadedObject:" + loadedObject.name);
+        yield return new WaitForSeconds(5);
+        GameObject instantiatedObject = Instantiate(loadedObject, structurePosition, Quaternion.identity);
+        StructureInfo script2 = instantiatedObject.GetComponent<StructureInfo>();
+        script2.X = (int)structurePosition.x;
+        script2.Y = (int)structurePosition.y;
+        //script.name = structure.name;
+        script2.Level = script.Level;
+        Destroy(gameObject);
     }
 }
