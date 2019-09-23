3da17def907fcc4f6b0e436d14ec4553e06e7e83
Jakov Jelenić <jakov.jelenic@fer.hr>
2019-09-23 22:02:03 +0200

    added most UI elements required for ship building

---
+++ Assets/Data/StructureData/ShipBuildingData.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//[System.Serializable]
+/*public class ShipBuildingData 
+{
+    public ShipData[] data;
+    public ShipSaveData(GameObject[] structures)
+    {
+        data = new ShipData[structures.Length];
+
+        for (int i = 0; i < structures.Length; i++)
+        {
+            StructureData structureData = new StructureData(structures[i]);
+            //Debug.Log("debug structrue saving " + i + ". " + structureData.name + "-::" + structureData.position[0] + "," + structureData.position[1] + "==== duration: " + structureData.duration );
+            data[i] = structureData;
+
+        }
+
+    }
+}*/
+
+[System.Serializable]
+public class ShipData
+{
+    public int componentNumber;
+    public int[] componentList;
+
+
+    public ShipData(int num)
+    {
+        componentNumber = num;
+        componentList = new int[num];
+
+    }
+
+    public void setComponent(int num, int id)
+    {
+        componentList[num] = id;
+    }
+
+    public int getComponent(int num)
+    {
+        return componentList[num];
+    }
+}
---
+++ Assets/Scenes/BaseScene/Scripts/ShipBuilding.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShipBuilding : MonoBehaviour
+{
+
+    public ShipData ship;
+    private int componentToChange;
+
+    public Text ComponentText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void newShip(int num)
+    {
+        ship = new ShipData(num);
+        Debug.Log("ship is new");
+    }
+
+    public void setComponentToChange(int num)
+    {
+        componentToChange = num;
+    }
+
+    public void changeComponent(int id)
+    {
+        ship.setComponent(componentToChange, id);
+    }
+
+    public void setComponentInfoText()
+    {
+        string textForTextView = "";
+        int num = 0;
+        Debug.Log("componentNumber:" + ship.componentNumber.ToString());
+        foreach (int i in ship.componentList){
+            textForTextView += (num + ":" + i + ";;");
+        }
+        Debug.Log("textToChange:" + textForTextView);
+        ComponentText.text = textForTextView;
+
+    }
+}
