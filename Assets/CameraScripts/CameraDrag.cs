c6e9c6dea8a096d8f32a9fb3b07a1e3436d911dc
Jakov Jelenić <jakov.jelenic@fer.hr>
2019-10-07 14:47:08 +0200

    added basic camera controls

---
+++ Assets/CameraScripts/CameraDrag.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDrag : MonoBehaviour
+{
+    private float dragSpeed;
+    private Vector2 dragOrigin;
+    private Vector3 pos;
+
+    private void Start()
+    {
+        pos = Camera.main.transform.position;
+        dragSpeed = 0.7f;
+    }
+
+
+    void LateUpdate()
+    {
+        /*if (Input.GetMouseButtonDown(0))
+        {
+            dragOrigin = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            return;
+        }
+
+        if (!Input.GetMouseButton(0)) return;
+
+        Vector2 pos = Camera.main.ScreenToViewportPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y) - dragOrigin);
+        Vector2 move = new Vector2(pos.x * dragSpeed, pos.y * dragSpeed);
+
+        transform.Translate(move, Space.World);*/
+
+        if (Input.GetMouseButton(2))
+        {
+             pos -= new Vector3(Input.GetAxis("Mouse X") * dragSpeed, Input.GetAxis("Mouse Y") * dragSpeed,0);
+        }
+        Camera.main.transform.position = pos;
+    }
+
+
+}
---
+++ Assets/CameraScripts/ZoomControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+
+        }
+        else
+        {
+            //up
+            if (Input.GetAxis("Mouse ScrollWheel") > 0)
+            {
+                if (GetComponent<Camera>().orthographicSize > 2)
+                {
+                    GetComponent<Camera>().orthographicSize = GetComponent<Camera>().orthographicSize - 0.2f;
+                }
+            }
+            if (Input.GetAxis("Mouse ScrollWheel") < 0)
+            {
+                if (GetComponent<Camera>().orthographicSize < 15)
+                {
+                    GetComponent<Camera>().orthographicSize = GetComponent<Camera>().orthographicSize + 0.2f;
+                }
+            }
+        }
+    }
+}
